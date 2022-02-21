3dd88ba3ece1716650b9ade1c2e0f8132f1d4986
DayanMonteiro <dmsmamb@gmail.com>
2022-02-21 07:18:13 -0300

    tipando componentes

--- Chapter-I/1_Fundamentos/Projeto/myskills/src/pages/Home.tsx
+++ Chapter-I/1_Fundamentos/Projeto/myskills/src/pages/Home.tsx
@@ -53,7 +53,10 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <Button onPress={handleAddNewSkill} />
+      <Button 
+      title="Add"
+      onPress={handleAddNewSkill}
+      />
 
       <Text style={[styles.title, {marginVertical: 20}]}>My Skills</Text>
 
