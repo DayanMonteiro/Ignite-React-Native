26284cbee9ebc4d8a8bd77148cd94d21a84181d1
DayanMonteiro <dmsmamb@gmail.com>
2022-02-28 14:21:05 -0300

    cartões de transações

--- Chapter-II/gofinance/src/screens/Dashboard/index.tsx
+++ Chapter-II/gofinance/src/screens/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
-
+import { TransactionCard } from '../../components/TransactionCard'
 
 import { 
     Container,
@@ -12,7 +12,9 @@ import {
     UserGreeting,
     UserName,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title
 } from './styles';
 
 export function Dashboard() {
@@ -52,6 +54,12 @@ export function Dashboard() {
                />
 
            </HighlightCards>
+
+           <Transactions>
+               <Title>Listagem</Title>
+
+               <TransactionCard />
+           </Transactions>
         </Container>
     )
 }
