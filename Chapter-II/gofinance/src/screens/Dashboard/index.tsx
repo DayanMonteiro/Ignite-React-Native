e37a92d16e0cc4371f5370036d4d1eb4b215b316
DayanMonteiro <dmsmamb@gmail.com>
2022-02-28 11:49:34 -0300

    acessando propriedades no styled components

--- Chapter-II/gofinance/src/screens/Dashboard/index.tsx
+++ Chapter-II/gofinance/src/screens/Dashboard/index.tsx
@@ -11,7 +11,8 @@ import {
     User,
     UserGreeting,
     UserName,
-    Icon
+    Icon,
+    HighlightCards
 } from './styles';
 
 export function Dashboard() {
@@ -30,7 +31,12 @@ export function Dashboard() {
                 </UserWrapper>
            </Header>
 
-           <HighlightCard />
+           <HighlightCards>
+               <HighlightCard />
+               <HighlightCard />
+               <HighlightCard />
+               <HighlightCard />
+           </HighlightCards>
         </Container>
     )
 }
