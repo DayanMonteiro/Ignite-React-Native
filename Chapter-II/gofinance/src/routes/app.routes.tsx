78ca0cb87da069f2648b7a753c460203ec7ab85a
DayanMonteiro <dmsmamb@gmail.com>
2022-04-01 16:08:59 -0300

    utilizando a tab navigation

--- Chapter-II/gofinance/src/routes/app.routes.tsx
+++ Chapter-II/gofinance/src/routes/app.routes.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import {Platform} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import {MaterialIcons} from '@expo/vector-icons';
+import {MaterialIcons} from '@expo/vector-icons';
 import {useTheme} from 'styled-components';
 
 import {Dashboard} from '../screens/Dashboard';
 import {Register} from '../screens/Register';
 // import {RFValue} from 'react-native-responsive-fontsize';
-// import {Platform} from 'react-native';
 // import {Resume} from '../screens/resume';
 
 const {Screen, Navigator} = createBottomTabNavigator();
 
 export function AppRoutes() {
-  const {colors, fonts} = useTheme();
-  /*foi possível pegar pela determinação em styled.d.ts  */
+    const theme = useTheme();
   return (
     <Navigator
+        screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: theme.colors.secondary,
+            tabBarInactiveTintColor: theme.colors.text,
+            tabBarLabelPosition: 'beside-icon',
+            tabBarStyle: {
+                height: 88,
+                paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+            }
+        }}
     //   tabBarOptions={{
     //     activeTintColor: colors.secondary,
     //     inactiveTintColor: colors.subTitle,
@@ -34,30 +43,30 @@ export function AppRoutes() {
       <Screen
         name="Listagem"
         component={Dashboard}
-        // options={{
-        //   tabBarIcon: ({size, color}) => (
-        //     <MaterialIcons size={size} color={color} name="menu" />
-        //   ),
-        // }}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialIcons size={size} color={color} name="format-list-bulleted" />
+          ),
+        }}
       />
       <Screen
         name="Cadastrar"
         component={Register}
-        // options={{
-        //   tabBarIcon: ({size, color}) => (
-        //     <MaterialIcons size={size} color={color} name="attach-money" />
-        //   ),
-        // }}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialIcons size={size} color={color} name="attach-money" />
+          ),
+        }}
       />
       <Screen
         name="Resumo"
         component={Register}
         // component={Resume}
-        // options={{
-        //   tabBarIcon: ({size, color}) => (
-        //     <MaterialIcons size={size} color={color} name="pie-chart" />
-        //   ),
-        // }}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialIcons size={size} color={color} name="pie-chart" />
+          ),
+        }}
       />
     </Navigator>
   );
